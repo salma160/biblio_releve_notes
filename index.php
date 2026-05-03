995960fb516a3ee79c00c7c95f2434d04be53dfa
salma <salmalechkar07@gmail.com>
2026-05-03 20:50:52 +0100

    initialisation de notes et de index avec ce son initialisation de variables

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'notes.php';
+
+$etudiants=[
+    ["prenom" => "Yassine","nom"=> "Elbouari", "notes" => [14,12,9,15,11]],
+    ["prenom" => "Mehdi","nom"=> "Tazi", "notes" => [17,18,15,16,19]]
+];
+
+$matieres=["Maths","Physique","Informatique","Anglais","Francais"];
+$seuil_admission=10;
